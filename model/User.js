535ae5e7b27d35163b60dc52a76e9202af62fec8
(nus)weikiat <weikiat.leong87@gmail.com>
2020-01-24 11:29:58 +0800

    Added new User Model
     Changes to be committed:
    	new file:   model/User.js
    	modified:   package.json
    	new file:   routes.js
    	modified:   server.js
    	modified:   yarn.lock

---
+++ model/User.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  name: { type: String, required: true },
+  role: { type: String, required: true }
+});
+
+module.exports = mongoose.model("User", User);
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+//const routes = require('./routes');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-app.use(cors);
+mongoose
+  .connect(process.env.DBURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .catch(err => console.error(err));
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+//app.use("/api",  routes);
 
 app.listen(port, () => {
-  console.log(`server listening on :${port}`);
+  console.log(`server started ${port}`);
 });
-
-app.get("/", (req, res) => { res.send('hello world')});
